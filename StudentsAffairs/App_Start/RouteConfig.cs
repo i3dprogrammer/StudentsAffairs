bfe33f993f0ba902f7aeee93fa169315c5e533e3
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2018-02-23 14:42:05 -0800

    sort by name

--- StudentsAffairs/App_Start/RouteConfig.cs
+++ StudentsAffairs/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace StudentsAffairs
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Students", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name:"Students",
+                url: "{controller}/{action}/{sortOrder}"
+                );
         }
     }
 }
